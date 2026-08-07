49f62c7bb9af4fa3a997dae1282217f8f3e80193
jms2163 <jms2163@email.vccs.edu>
2026-08-07 11:31:41 -0400

    Update GameState.js

--- scr/app/GameState.js
+++ scr/app/GameState.js
@@ -1,1 +1,36 @@
 
+const gameState = {
+    player: {
+        id: null,
+        name: "",
+    },
+
+    registry: {
+        inventory: [],
+        discoveries: [],
+        certifications: [],
+        quests: [],
+        journal: [],
+    },
+
+    zones: {
+        pond: {},
+        quantum: {},
+        atomLab: {},
+        atomizer: {},
+        molecularizer: {},
+        macromolecularizer: {},
+        polymerizer: {},
+        metabolism: {},
+        genetics: {},
+    },
+
+    settings: {
+        volume: 1,
+        difficulty: "normal",
+    },
+
+    saveVersion: 1,
+};
+
+export default gameState;
