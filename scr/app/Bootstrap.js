255f9ecec4ecdccd75a2fb38de7e9a9cf3ab099f
jms2163 <jms2163@email.vccs.edu>
2026-08-07 14:15:22 -0400

    Update Bootstrap.js

--- scr/app/Bootstrap.js
+++ scr/app/Bootstrap.js
@@ -1,93 +1,19 @@
-// --------------------------------------------------
-// Bootstrap.js
-// Responsible for initializing the entire game
-// --------------------------------------------------
+import BootstrapUI from "./BootstrapUI.js";
 
-//import DataLoader from "./DataLoader.js";
-//import GameStateManager from "./GameStateManager.js";
-//import GameStateObserver from "./GameStateObserver.js";
-//import FacilityUI from "./FacilityUI.js";
-//import ZoneManager from "./ZoneManager.js";
-//import SimulationClock from "./SimulationClock.js";
-//import BootstrapUI from "./BootstrapUI.js";
 
 const Bootstrap = {
 
-    // --------------------------------------------------
-    // Main entry point
-    // --------------------------------------------------
-    async initialize() {
+    async initialize(){
+
         BootstrapUI.initialize();
 
-        await this.loadData();
-        BootstrapUI.mark("Data");
-
-        await this.initializeGameState();
-        BootstrapUI.mark("Game State");
-
-        await this.initializeObservers();
-        BootstrapUI.mark("Observers");
-
-        await this.initializeUI();
-        BootstrapUI.mark("UI");
-
-        await this.initializeZones();
-        BootstrapUI.mark("Zones");
-
-        await this.startSimulation();
-        BootstrapUI.mark("Simulation");
+        BootstrapUI.mark("Bootstrap loaded");
 
         BootstrapUI.ready();
-    },
 
-
-    // --------------------------------------------------
-    // Load Data
-    // --------------------------------------------------
-    async loadData() {
-        await DataLoader.loadAll();
-    },
-
-
-    // --------------------------------------------------
-    // Initialize Game State
-    // --------------------------------------------------
-    async initializeGameState() {
-        GameStateManager.reset(); // or hydrate from save
-    },
-
-
-    // --------------------------------------------------
-    // Initialize Observers
-    // --------------------------------------------------
-    async initializeObservers() {
-        // Example:
-        // GameStateObserver.on("tick", () => UIManager.updateTick());
-    },
-
-
-    // --------------------------------------------------
-    // Initialize UI
-    // --------------------------------------------------
-    async initializeUI() {
-        UIManager.initialize();
-    },
-
-
-    // --------------------------------------------------
-    // Initialize Zones
-    // --------------------------------------------------
-    async initializeZones() {
-        ZoneManager.initialize();
-    },
-
-
-    // --------------------------------------------------
-    // Start Simulation Clock
-    // --------------------------------------------------
-    async startSimulation() {
-        SimulationClock.start();
     }
+
 };
+
 
 export default Bootstrap;
